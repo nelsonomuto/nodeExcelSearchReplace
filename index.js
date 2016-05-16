fa1886cbd2c5665c3d8ec58a389655fdf2edc959
Nelson Omuto <nelsonomuto@gmail.com>
2016-05-16 17:16:42 -0400

    test input to output file will be created when index is ran

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-XLSX = require('xlsx');
-var workbook = XLSX.readFile('test.xlsx');
+var XLSX = require('xlsx');
+var workbook = XLSX.readFile('input.xls');
+var data = JSON.parse(require('fs').readFileSync('data.json', 'utf8'));
+
+
+function Workbook() {
+    if(!(this instanceof Workbook)) return new Workbook();
+    this.SheetNames = [];
+    this.Sheets = {};
+}
+
+var newWb = new Workbook();
+
+var sheet_name_list = workbook.SheetNames;
+
+
+sheet_name_list.forEach(function(y) { /* iterate through sheets */
+    var worksheet = workbook.Sheets[y];
+
+    newWb.SheetNames.push(y);
+    newWb.Sheets[y] = worksheet;
+
+    for (var z in worksheet) {
+        /* all keys that do not begin with "!" correspond to cell addresses */
+        if(z[0] !== '!') {
+            console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+            for(var replaceKey in data) {
+                if(worksheet[z].v.match(replaceKey) !== null) {
+                    worksheet[z].v = data[replaceKey];
+                }
+            }
+        };
+    }
+});
+
+XLSX.writeFile(newWb, 'output.xlsx');
